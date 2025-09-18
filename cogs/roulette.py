77cff4516751d0961c235adcf65bab104ec58496
manonlortal-sys <manonlortal@gmail.com>
2025-09-18 11:48:28 +0200

    Update roulette.py

--- cogs/roulette.py
+++ cogs/roulette.py
@@ -1,1 +1,14 @@
+import discord
+from discord.ext import commands
 
+class Roulette(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="testroulette")
+    async def testroulette(self, ctx):
+        """Commande de test pour vérifier que le cog roulette fonctionne"""
+        await ctx.send("🎰 Le cog Roulette est bien chargé !")
+
+async def setup(bot):
+    await bot.add_cog(Roulette(bot))
