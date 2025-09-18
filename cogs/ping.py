f0742a7674ad61a71e5b5ce5409e9c5b184c31d9
manonlortal-sys <manonlortal@gmail.com>
2025-09-18 11:48:44 +0200

    Update ping.py

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,1 +1,14 @@
+import discord
+from discord.ext import commands
 
+class Ping(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="testping")
+    async def testping(self, ctx):
+        """Commande de test pour vérifier que le cog ping fonctionne"""
+        await ctx.send("📢 Le cog Ping est bien chargé !")
+
+async def setup(bot):
+    await bot.add_cog(Ping(bot))
