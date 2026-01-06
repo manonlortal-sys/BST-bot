e3cbafbc910bd9c357e3956e4e9bafa3558411c5
manonlortal-sys <manonlortal@gmail.com>
2026-01-06 15:56:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,25 @@ from discord.ext import commands
 INTENTS = discord.Intents.default()
 INTENTS.message_content = True
 
-bot = commands.Bot(
-    command_prefix="!",  # obligatoire même si inutilisé
-    intents=INTENTS
-)
+
+class CafardBot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+            command_prefix="!",
+            intents=INTENTS
+        )
+
+    async def setup_hook(self):
+        await self.load_extension("cogs.cafard")
+        await self.tree.sync()
+
+
+bot = CafardBot()
+
 
 @bot.event
 async def on_ready():
     print(f"Connecté en tant que {bot.user}")
-    await bot.tree.sync()
 
-async def setup():
-    await bot.load_extension("cogs.cafard")
-
-bot.loop.create_task(setup())
 
 bot.run(os.getenv("DISCORD_TOKEN"))
