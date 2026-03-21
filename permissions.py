4d9a7030d11cab1b1470c12ae2cf1475f346f7d0
manonlortal-sys <manonlortal@gmail.com>
2026-03-21 10:50:14 +0100

    Fix role name case sensitivity for 'Lead'

--- permissions.py
+++ permissions.py
@@ -5,7 +5,7 @@ def is_orga_or_admin(interaction):
 
     # Rôle Lead
     for role in interaction.user.roles:
-        if role.name.lower() == "lead":
+        if role.name.lower() == "Lead":
             return True
 
     return False
