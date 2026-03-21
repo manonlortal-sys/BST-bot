070b158a620eb32778737e219b2d831696ddd710
manonlortal-sys <manonlortal@gmail.com>
2026-03-21 11:15:10 +0100

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ CHANNEL_EMBEDS_ID = 1484650057935622144
 CLASSES = [
     "iop", "cra", "eniripsa", "xelor", "feca",
     "sacrieur", "pandawa", "osamodas", "ecaflip",
-    "enutrof", "sram", "steamer", "eliotrope",
-    "huppermage", "ouginak", "zobal"
+    "enutrof", "sram", "steamer", "sadida",
+    "roublard", "zobal"
 ]
