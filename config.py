7f3f2f89f8377847b3b2e2c83385445f981a90ca
manonlortal-sys <manonlortal@gmail.com>
2026-03-21 10:46:47 +0100

    Add CHANNEL_EMBEDS_ID and CLASSES list to config

---
+++ config.py
@@ -0,0 +1,8 @@
+CHANNEL_EMBEDS_ID = 1484650057935622144
+
+CLASSES = [
+    "iop", "cra", "eniripsa", "xelor", "feca",
+    "sacrieur", "pandawa", "osamodas", "ecaflip",
+    "enutrof", "sram", "steamer", "eliotrope",
+    "huppermage", "ouginak", "zobal"
+]
