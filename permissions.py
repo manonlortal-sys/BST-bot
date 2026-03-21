cdeb6182ffc7f9d75cacdd52ebc868a6d2bf73eb
manonlortal-sys <manonlortal@gmail.com>
2026-03-21 10:47:10 +0100

    Add is_orga_or_admin function for permission checks

---
+++ permissions.py
@@ -0,0 +1,11 @@
+def is_orga_or_admin(interaction):
+    # Admin
+    if interaction.user.guild_permissions.administrator:
+        return True
+
+    # Rôle Lead
+    for role in interaction.user.roles:
+        if role.name.lower() == "lead":
+            return True
+
+    return False
