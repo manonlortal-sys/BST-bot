548f518709e7f5aad4a987b45122a5b77436d183
manonlortal-sys <manonlortal@gmail.com>
2026-04-07 11:41:31 +0200

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-CHANNEL_EMBEDS_ID = 1484650057935622144
-
-CLASSES = [
-    "iop", "cra", "eniripsa", "xelor", "feca",
-    "sacrieur", "pandawa", "osamodas", "ecaflip",
-    "enutrof", "sram", "steamer", "sadida",
-    "roublard", "zobal"
-]
